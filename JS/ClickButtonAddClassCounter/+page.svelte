697d36ca0086146db3113dbc24c5ec0cdd1acdf0
PantaKoda <147987379+PantaKoda@users.noreply.github.com>
2024-05-12 15:37:42 +0200

    Create +page.svelte
    Dynamically add a div box in a grid layout.
    Has counter to add naming one the div boxes.

---
+++ JS/ClickButtonAddClassCounter/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	let count = 1;
+
+	function addDivBox() {
+		const gridContainer = document.querySelector('.grid-container');
+		const newBox = document.createElement('div');
+		newBox.className = `box-${count}`;
+		newBox.textContent = `BOX-${count}`;
+		newBox.style.border = '2px solid blue';
+		gridContainer?.appendChild(newBox);
+		count++;
+	}
+</script>
+
+<div class="grid-container"></div>
+<button id="btn" on:click={addDivBox}> add div box </button>
+
+<style>
+	.grid-container {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		border: 2px solid red;
+	}
+</style>
